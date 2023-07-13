bf38a29a73b12d76aa2496befaddff412ea62f63
Shahd Ammar <shahdammar413@gmail.com>
2023-07-13 13:38:45 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
- *puts function program 
+/**
+ * main - this is the entry point of the program
+ * Return: always zero
  */
 
 int main(void)
 {
-	/**
-	 *This's main function
-	 **/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
