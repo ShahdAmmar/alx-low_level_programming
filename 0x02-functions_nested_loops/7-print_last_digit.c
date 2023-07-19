f6c2a888188e86349a405a1d7556a89f6473af08
Shahd Ammar <shahdammar413@gmail.com>
2023-07-19 05:39:56 -0700

    7-last_digit commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,11 @@ int print_last_digit(int n)
 {
 	int digit;
 
-	if (n >= 0)
+	digit = n % 10;
+	if (digit < 0)
 	{
-		digit = n % 10;
-		_putchar(digit + 48);
-		return (digit);
+		digit *= -1;
 	}
-	else
-	{
-		digit = (n / -1) % 10;
-		_putchar(digit + 48);
-		return (digit);
-	}
+	_putchar(digit + 48);
+	return (digit);
 }
