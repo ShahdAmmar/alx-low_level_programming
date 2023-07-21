4c5a06adbb6129b979ecc57945d29ffc1e7dbf7e
Shahd Ammar <shahdammar413@gmail.com>
2023-07-20 17:17:27 -0700

    4 & 5 commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10x
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + 48);
+			j++;
+		}
+		_putchar('\n');
+	}
+}
+
