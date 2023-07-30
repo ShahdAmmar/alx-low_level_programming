b6439ab1367d21b35701156ffbeefe5052872424
Shahd Ammar <shahdammar413@gmail.com>
2023-07-30 09:09:36 -0700

    2-strncpy.c commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,9 @@
 /**
- * *_strncpy - copy n bytes 
+ * *_strncpy - copy n bytes
  * @dest: where the string is copied
  * @src: what should be copied
  * @n: No. bytes
+ * Return: resulting dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
