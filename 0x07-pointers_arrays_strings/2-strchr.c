8cb4be0dfb4719fda20d860763d82369bb795ccc
Shahd Ammar <shahdammar413@gmail.com>
2023-08-01 06:16:42 -0700

    2-strchr.c commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * *_strchr - search for char
+ * @s: string array
+ * @c: char
+ * Return: 1st occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
