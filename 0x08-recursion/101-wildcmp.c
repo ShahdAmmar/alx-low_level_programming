920988f0a72c53a562f505c9963cda463cde546d
Shahd Ammar <shahdammar413@gmail.com>
2023-08-03 11:19:51 -0700

    101-wildcmp commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,63 @@
+/**
+ * move_past_star - return string s2 without stars
+ * @s2: strinf
+ * Return: s2 without *
+ */
+int wildcmp(char *s1, char *s2);
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+/**
+ * inception - compare s1 and s2
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: ret value
+ */
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+/**
+ * wildcmp - compare
+ * @s1: 1st string
+ * @s2: 2nd string 
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
