f4a01efccff47d128654686274caa35b430219cf
Shahd Ammar <shahdammar413@gmail.com>
2023-08-04 08:56:53 -0700

    4-add.c commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add positive numbers
+ * @argc: count
+ * @argv: vector
+ * Return: 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= 48 && *argv[i] <= 57)
+		{
+			if (atoi(argv[i]) > 0)
+				sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
