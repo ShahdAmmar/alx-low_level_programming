daedd00179baa8f86866db6b9a24c5a8ab91d372
Shahd Ammar <shahdammar413@gmail.com>
2023-08-08 16:11:07 -0700

    2-str_concat.c commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,9 +29,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 	if (m == 0)
 		return (0);
 
-	for (i = 0; i < size1 + size2; i++)
+	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
 			m[i] = s1[i];
