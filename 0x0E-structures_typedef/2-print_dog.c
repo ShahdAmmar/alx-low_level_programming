fc5cf71813cec607051d779e225d27476bfa0b23
Shahd Ammar <shahdammar413@gmail.com>
2023-08-15 06:52:01 -0700

    2-print_dog.c commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog data
+ * @d: struct dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("nil\n");
+		if (d->age != 0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("nil\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("nil\n");
+	}
+}
