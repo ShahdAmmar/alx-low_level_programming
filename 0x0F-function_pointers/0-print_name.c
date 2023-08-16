8ab386242ce6463c453f74275fb8e57b1666ef5d
Shahd Ammar <shahdammar413@gmail.com>
2023-08-16 12:51:39 -0700

    print_name commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - print name in specific form
+ * @name: person nam
+ * @f: pointer to func
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
