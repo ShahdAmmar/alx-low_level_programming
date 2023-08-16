29567333709485d5837dadbc9b7e5fe7ea306f0b
Shahd Ammar <shahdammar413@gmail.com>
2023-08-16 13:17:24 -0700

    int_index commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - return index of specific num
+ * @array: array of int
+ * @size: length of array
+ * @cmp: func
+ * Return: index of -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) == 1)
+					return (i);
+		}
+	}
+	return (-1);
+}
