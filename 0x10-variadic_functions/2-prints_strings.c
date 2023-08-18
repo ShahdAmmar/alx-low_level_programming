9309a70a4529b0ac2ba6fc8ee3ce467b8fa4ce22
Shahd Ammar <shahdammar413@gmail.com>
2023-08-18 00:30:59 -0700

    print_strings commit

---
+++ 0x10-variadic_functions/2-prints_strings.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print numbers given as arguments
+ * @separator: what is printed bet numbers
+ * @n: number of optional args
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+        unsigned int i;
+        va_list args;
+
+        va_start(args, n);
+        for (i = 0; i < n; i++)
+        {
+                s = va_arg(args, char);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+                if (i != (n - 1) && separator != NULL)
+                        printf("%s", separator);
+        }
+        printf("\n");
+        va_end(args);
+}
+~         
