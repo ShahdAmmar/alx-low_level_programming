ea8df8a45ee4352ef66ac0aa8539ad65ee65c602
ShahdAmmar <shahdammar413@gmail.com>
2023-08-24 14:51:39 -0700

    add_node commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - add node at beginning
+ * @head: ptr to head node
+ * @str: string
+ * Return: ptr to new element or NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_hd = malloc(sizeof(list_t));
+
+	if (head == NULL || new_hd == NULL)
+		return (NULL);
+
+	if (str)
+	{
+		new_hd->str = strdup(str);
+		if (!new_hd->str)
+		{
+			free(new_hd);
+			return (NULL);
+		}
+		new_hd->len = strlen(new_hd->str);
+	}
+	new_hd->next = *head;
+	*head = new_hd;
+	return (new_hd);
+}
