5d77223be0d56c17beb188656eec272774aaac58
ShahdAmmar <shahdammar413@gmail.com>
2023-08-29 04:50:27 -0700

    free listint2 commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *nxt_nd;
 
-	if (head == NULL || *head == NULL)
+	if (!head || !(*head))
 		return;
+
 	while (*head)
 	{
 		nxt_nd = (*head)->next;
 		free(*head);
 		*head = nxt_nd;
 	}
-	/*free(*head);*/
 	*head = NULL;
 }
