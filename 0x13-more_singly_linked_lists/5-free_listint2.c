d347f1975eba543c6444b9d8b12bc76bedafef4b
ShahdAmmar <shahdammar413@gmail.com>
2023-08-29 04:35:39 -0700

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list and set head to null
+ * @head: ptr to head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *nxt_nd;
+
+	while (*head)
+	{
+		nxt_nd = (*head)->next;
+		free(*head);
+		*head = nxt_nd;
+	}
+	*head = NULL;
+}
