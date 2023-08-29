4ec6ef0afff7e645c9902955336842c8c2ae26f7
ShahdAmmar <shahdammar413@gmail.com>
2023-08-29 06:45:41 -0700

    pop_listint commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: head node
+ * Return: head node's data or 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	int h_data;
+	listint_t *temp;
+	
+	if (!head || !(*head))
+		return (0);
+
+	h_data = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (h_data);
+}
