a6648170d39d87c56b9afeff72fe144075e1e35d
ShahdAmmar <shahdammar413@gmail.com>
2023-08-31 09:43:08 -0700

    3-set_bit.c commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+/**
+ * set_bit - set specific index to 1
+ * @n: decimal number
+ * @index: specific indwx
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (*n |= 1 << index);
+}
