2b7f27a68d0aac756262e1e703cd52fb26e6d675
ShahdAmmar <shahdammar413@gmail.com>
2023-09-07 06:55:05 -0700

    1-create_file.c commit1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int slen = 0;
+
+	while (*s != '\0')
+	{
+		slen++;
+		s++;
+	}
+	return (slen);
+}
+
+/**
+ * create_file - create a file which may contains text
+ * @filename: name of created file
+ * @text_content: text to put in the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, slen = 0, w_data;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+		slen = _strlen(text_content);
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w_data = write(file, text_content, slen);
+	if (file == -1 || w_data == -1)
+		return (-1);
+	close(file);
+	return(1);
+}
