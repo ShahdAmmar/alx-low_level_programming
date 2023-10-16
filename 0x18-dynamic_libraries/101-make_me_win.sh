76168adb178658fdfb7e041fd9e54259a23e4d0b
ShahdAmmar <shahdammar413@gmail.com>
2023-10-16 13:56:24 -0700

    101-make_me_win.sh commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/ShahdAmmar/alx-low_level_programming/blob/d037137143233695f63155e1ceb120a59ef74df8/0x18-dynamic_libraries/libgiga.so
+wget -P https://github.com/ShahdAmmar/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_RELOAD=/tmp/libgiga.so
