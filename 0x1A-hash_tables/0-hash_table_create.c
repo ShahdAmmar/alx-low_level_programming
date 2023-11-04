4d581d8c2426c2f735bbe46250fce5527ac0fa7c
ShahdAmmar <shahdammar413@gmail.com>
2023-11-03 17:39:49 -0700

    creating hash table commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creat ht
+ * @size: size of ht
+ * Return: ht
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *hsh_tbl;
+
+	hsh_tbl = malloc(sizeof(hash_table_t));
+	if (hsh_tbl == NULL)
+		return (NULL);
+
+	hsh_tbl->size = size;
+	hsh_tbl->array = malloc(size * sizeof(hash_node_t));
+	if (hsh_tbl->array == NULL)
+	{
+		free(hsh_tbl);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		hsh_tbl->array[i] = NULL;
+
+	return (hsh_tbl);
+}
